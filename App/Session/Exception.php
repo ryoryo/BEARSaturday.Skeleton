a5c57b456ba502c513b15d4bbe0f581ad5856575
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-06-14 20:11:47 +0900

    fix cs

--- App/Session/Exception.php
+++ App/Session/Exception.php
@@ -1,13 +1,6 @@
 <?php
 
 
-/**
- * App_Session_Exception
- *
- * @license @license@ http://@license_url@
- *
- * @link http://@link_url@
- */
 class App_Session_Exception extends BEAR_Exception
 {
 }
